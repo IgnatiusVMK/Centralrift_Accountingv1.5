a41cc726b32886316f18e71f58970be0e65e6977
IgnatiusVMK <ignatiusvmk@gmail.com>
2025-04-09 02:54:46 +0300

    09-04-2025 Update 3 1. Migration to add remarks column to credits table in production database;

---
+++ database/migrations/2025_04_09_025328_add_remarks_column_to_credits_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('credits', function (Blueprint $table) {
+            $table->string('Remarks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('credits', function (Blueprint $table) {
+            //
+        });
+    }
+};
